1ce7199bae6868a50e2f01245a4bfe8e455258d4
thiomajid <tchiombianomajid@gmail.com>
2024-12-08 16:25:36 +0100

    Remove publications from RSS feed generation to streamline data retrieval

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,10 +3,9 @@ import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-  const [posts, projects, publications] = await Promise.all([
+  const [posts, projects] = await Promise.all([
     getCollection("blog"),
     getCollection("projects"),
-    getCollection("publications"),
   ]);
 
   const blogItems = posts
@@ -23,15 +22,10 @@ export async function GET(context) {
       link: `/projects/${project.slug}/`,
     }));
 
-  const publicationItems = publications.map((publication) => ({
-    ...publication.data,
-    link: `/publications/${publication.slug}/`,
-  }));
-
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: publicationItems.concat(projectItems, blogItems),
+    items: projectItems.concat(blogItems),
   });
 }
