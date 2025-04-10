0d26154fa74dd3508773023f3b0e864b995c8ab1
thiomajid <tchiombianomajid@gmail.com>
2025-04-10 05:03:55 +0100

    Reworked some layouts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -23,6 +23,9 @@ export default {
         "neo-hover": "6px 6px 0px 0px rgba(0,0,0,0.9)",
         "neo-light": "4px 4px 0px 0px rgba(255,255,255,0.4)", // Further reduced opacity for softer shadows
       },
+      borderWidth: {
+        3: "3px",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
